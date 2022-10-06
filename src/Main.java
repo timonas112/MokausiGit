91ea10e38c42f7e1074d4c5cf5f2b2492a309b52
Tim <timonasangelis@gmail.com>
2022-10-06 19:24:40 +0300

    hello

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int i = 1;
+        int a = 2;
+        int as = a + i;
+        System.out.println(as);
     }
 }
